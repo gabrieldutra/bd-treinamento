88a217dd1e3811cc8cf0b79f2d4163ce5bdbe837
Gabriel Dutra <nesk.frz@gmail.com>
2018-09-21 11:12:33 -0300

    1st exercise + expected results

---
+++ 1.sql
@@ -0,0 +1,26 @@
+-- 1. Selecione o nome e sobrenome dos empregados
+-- que já tiveram cargo “Accountant”.
+
+SELECT E.nm_empregado, E.nm_sobrenome
+FROM empregado E
+WHERE EXISTS(SELECT 1 FROM historico_emprego H WHERE H.nu_empregado = E.nu_empregado AND H.nm_cargo = 'Accountant');
+
+-- Expected Output
+-- 
+-- 1.
+
+-- "Elizabeth Barbara";"Jones"
+-- "Robert";"Smith"
+-- "Allan";"White"
+-- "Gordon";"Reid"
+-- "Claire";"MacCallan"
+-- "Brian Charles";"Murphy"
+-- 
+-- Output
+-- 
+--  Elizabeth Barbara | Jones
+--  Robert            | Smith
+--  Allan             | White
+--  Gordon            | Reid
+--  Claire            | MacCallan
+--  Brian Charles     | Murphy
